b40740b9e7697c3b3bb2adeb69c8b2ab36bd07d2
assrat007 <asratadane169@gmail.com>
2023-08-06 23:00:28 +0300

    font added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,14 @@ module.exports = {
   content: ["./build/*.html"],
   theme: {
     extend: {
+      fontFamily:{
+        outfit:['outfit', 'san-serif'],
+      }
    
     },
     colors:{
       'Grayishblue':'#D5E1EF',
-      Lightgray :'hsl(212, 45%, 89%)',
+      Lightgray :'rgb(123, 135, 157)',
       Darkblue:' hsl(218, 44%, 22%)',
       shadow:' hsl(220, 15%, 55%)'
     }
