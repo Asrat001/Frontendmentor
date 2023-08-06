781f449139366aff1bc806c254df603542f0a49d
assrat007 <asratadane169@gmail.com>
2023-08-06 21:02:37 +0300

    final design

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.html"],
+  theme: {
+    extend: {
+   
+    },
+    colors:{
+      'Grayishblue':'#D5E1EF',
+      Lightgray :'hsl(212, 45%, 89%)',
+      Darkblue:' hsl(218, 44%, 22%)',
+      shadow:' hsl(220, 15%, 55%)'
+    }
+  },
+  plugins: [],
+}
