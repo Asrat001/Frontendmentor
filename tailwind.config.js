ded3ca83eaaaf35fcd6b5ed257e1fa670ee6d708
assrat007 <asratadane169@gmail.com>
2023-08-09 18:23:53 +0300

    tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./build/*.{html,js}"],
+  content: ["./build/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        Robotobold:"Roboto-bold",
+        Robotoregular:"Roboto-regular",
+      },
+      colors:{
+        white:"#FFFEFE",
+        btncolor:"#232742"
+
+      }
+      
+    },
+  
   },
+  
   plugins: [],
 }
+
